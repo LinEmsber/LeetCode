3a83cd3a355d7b9b46101946825fc15e6081603c
Lin Emsber <linemsber@gmail.com>
2017-06-14 23:45:17 +0800

    Review and add athe clear explanation.
    1. Add the clear explanation.
    2. Check the coding style.

--- Convert_a_Number_to_Hexadecimal_01.c
+++ Convert_a_Number_to_Hexadecimal_01.c
@@ -10,17 +10,24 @@ char * toHex(int num)
 	int tmp;
 	unsigned int * unum = &num;
 
-	char * res = (char *)malloc(9);
+	/* Include one byte for null byte and set it. */
+	char * res = (char *) malloc(8 + 1);
 	res[8] = 0;
 
+	/* The given number is guaranteed to fit within the range of a 32-bit signed integer.
+	 * 1. We copy the last significant 4 bits of the integer.
+	 * 2. Convert this 4 bits into hexadecimal and store it into the right side of the char array.
+	 * 3. The given number is right shifted 4 bits.
+	 */
 	while( *unum ){
 		tmp = *unum & 0x0000000f;
-		*(res+7-i) = tmp + ((tmp>9)?('a'-10):'0');
+		*(res + 7 - i) = tmp + ( (tmp > 9) ? ('a'-10) : '0' );
 		*unum = *unum >> 4;
 		i++;
 	}
 
-	return res+7-i+1;
+	/* We only need to return the most left position of the char array which is stored number. */
+	return res + 7 - i + 1;
 }
 
 int main()
