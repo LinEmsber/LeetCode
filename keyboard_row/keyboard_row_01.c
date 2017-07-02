623bba5cc34c509d6745f3d9e7ee1f145e198cd1
Lin Emsber <linemsber@gmail.com>
2017-07-02 23:38:47 +0800

    Review and improve the source code.

--- keyboard_row/keyboard_row_01.c
+++ keyboard_row/keyboard_row_01.c
@@ -1,21 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int is_in_row(char * row, int length, char row_3)
+int is_in_row(char * row, int row_len, char letter)
 {
-	int i;
-	int alphabet;
-	for (i = 0; i < length; i++) {
+	int i, alphabet;
+	for (i = 0; i < row_len; i++) {
 		alphabet = row[i];
-		if (alphabet == row_3 || (alphabet == row_3 + 32)) {
+		/* For lowercase and uppercase. */
+		if (alphabet == letter || (alphabet == letter + 32)) {
 			return 1;
 		}
 	}
 	return 0;
 }
 
-char** findWords(char** words, int wordsSize, int* returnSize)
+char ** findWords(char ** words, int wordsSize, int * returnSize)
 {
 	char row_1[10] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
 	char row_2[9] = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
@@ -23,11 +27,11 @@ char** findWords(char** words, int wordsSize, int* returnSize)
 
 	int i, j;
 	int row, is_in_one_row;
-	char * word;
 	int size = 0;
+	char * word;
 	char ** rows = (char **) malloc(sizeof(char *) * wordsSize);
 
-	for (int i = 0; i < wordsSize; i++) {
+	for (i = 0; i < wordsSize; i++) {
 
 		word = words[i];
 		j = 0;
@@ -36,28 +40,30 @@ char** findWords(char** words, int wordsSize, int* returnSize)
 
 		while (word[j] != '\0') {
 
+			/* Start from the first letter, and check the row of the first letter. */
 			if (row == 0) {
 
 				if (is_in_row(row_1, 10, word[j])) {
 					row = 1;
-					continue;
 				}
 				if (is_in_row(row_2, 9, word[j])) {
 					row = 2;
-					continue;
 				}
 				if (is_in_row(row_3, 7, word[j])) {
 					row = 3;
-					continue;
 				}
+				continue;
 
-			} else {
-
-				if ((is_in_row(row_1, 10, word[j]) && row != 1) || (is_in_row(row_2, 9, word[j]) && row != 2) || (is_in_row(row_3, 7, word[j]) && row != 3)) {
-					is_in_one_row = 0;
-					break;
-				}
-
+			/* After we ensure the row, we only need to check the specific row. */
+			} else if ( row == 1 && (is_in_row(row_1, 10, word[j]) != 1) ) {
+				is_in_one_row = 0;
+				break;
+			} else if ( row == 2 && (is_in_row(row_2, 9, word[j]) != 1) ) {
+				is_in_one_row = 0;
+				break;
+			} else if ( row == 3 && (is_in_row(row_3, 7, word[j]) != 1) ) {
+				is_in_one_row = 0;
+				break;
 			}
 			j++;
 		}
@@ -68,9 +74,37 @@ char** findWords(char** words, int wordsSize, int* returnSize)
 		}
 
 	}
-	
+
 	*returnSize = size;
 	return rows;
 }
 
 
+int main()
+{
+	int i;
+	int return_size;
+
+	char ** result_words;
+	char ** input_words = (char **) malloc( sizeof(char *) * 5);
+
+	input_words[0] = strdup("Hello");
+	input_words[1] = strdup("Alaska");
+	input_words[2] = strdup("Dad");
+	input_words[3] = strdup("Peace");
+	input_words[4] = strdup("You");
+
+
+	result_words = findWords(input_words, 5, &return_size);
+
+	for (i = 0; i < return_size; i++)
+		printf("result_words: %s\n", result_words[i]);
+
+	for (i = 0; i < 5; i++)
+		free(input_words[i]);
+
+	free(input_words);
+	free(result_words);
+
+	return 0;
+}
