e8753bc373c5b22a1b7fa382113771c42b2fedbe
Lin Emsber <linemsber@gmail.com>
2017-07-03 22:09:13 +0800

    Review and add example source code.

--- island_perimeter/island_perimeter_01.c
+++ island_perimeter/island_perimeter_01.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int islandPerimeter(int** grid, int gridRowSize, int gridColSize)
 {
         if ( grid == NULL || gridRowSize == 0 || gridColSize == 0 ){
@@ -13,26 +17,98 @@ int islandPerimeter(int** grid, int gridRowSize, int gridColSize)
 
                         if ( grid[i][j] == 1 ) {
 
-                                // initial primeter
-                                int num = 4;
+                                /* Initial primeter of one grid. */
+                                num = 4;
 
-                                // check up
-                                if ( (i > 0 && grid[i-1][j] > 0) )
+                                /* Check if the left side grid is exist or not. */
+                                if ( i > 0 && grid[i-1][j] > 0 )
                                         num--;
-                                // check left
-                                if ( j > 0 && grid[i][j-1] > 0)
+                                /* Check if the down side grid is exist or not. */
+                                if ( j > 0 && grid[i][j-1] > 0 )
                                         num--;
 
-                                // check the next row exist, and check down
+                                /* Check if the right side grid is exist or not. */
                                 if ( i < gridRowSize-1 && grid[i+1][j] > 0 )
                                         num--;
 
-                                // check the next column exist, and check right
+                                /* Check if the up side grid is exist or not. */
                                 if ( j < gridColSize-1 && grid[i][j+1] > 0 )
                                         num--;
+
                                 perimeter += num;
                         }
                 }
         }
         return perimeter;
+}
+
+
+void print_matrix(int ** matrix, int row, int col)
+{
+	int i, j;
+
+	for ( i = 0; i < row; i++){
+
+		for ( j = 0; j < col; j++ ){
+			printf("%d ", matrix[i][j] );
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+/* A safe free function.  */
+void _safe_free(void **pp)
+{
+	if (pp != NULL && *pp != NULL){
+		free(*pp);
+		*pp = NULL;
+	}
+}
+#define safe_free(p) _safe_free( (void**) &p )
+
+
+void free_matrix( int ** matrix, int row)
+{
+	int i;
+	for (i = 0; i < row; i++)
+		safe_free(matrix[i]);
+
+	safe_free(matrix);
+}
+
+int main()
+{
+        int i;
+        int row = 4;
+        int col = 4;
+
+        int ** grid = (int **) malloc( sizeof(int *) * row );
+        for (i = 0; i < row; i++)
+                grid[i] = (int *) malloc( sizeof(int) * col );
+
+        grid[0][0] = 0;
+        grid[0][1] = 1;
+        grid[0][2] = 0;
+        grid[0][3] = 0;
+        grid[1][0] = 1;
+        grid[1][1] = 1;
+        grid[1][2] = 1;
+        grid[1][3] = 0;
+        grid[2][0] = 0;
+        grid[2][1] = 1;
+        grid[2][2] = 0;
+        grid[2][3] = 0;
+        grid[3][0] = 1;
+        grid[3][1] = 1;
+        grid[3][2] = 0;
+        grid[3][3] = 0;
+
+        print_matrix(grid, row, col);
+
+        printf("perimeter: %d\n", islandPerimeter(grid, row, col));
+
+        free_matrix(grid, row);
+
+        return 0;
 }
--- island_perimeter/island_perimeter_02.c
+++ island_perimeter/island_perimeter_02.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 int islandPerimeter(int** grid, int gridRowSize, int gridColSize)
 {
         if ( grid == NULL || gridRowSize == 0 || gridColSize == 0 ){
@@ -13,18 +18,88 @@ int islandPerimeter(int** grid, int gridRowSize, int gridColSize)
 
                         if ( grid[i][j] == 1 ) {
 
-                                // add 1
                                 perimeter++;
 
-                                // check the next row exist, and check down
+                                /* Check if the right side grid is exist or not. */
                                 if ( i < gridRowSize-1 && grid[i+1][j] == 1 )
                                         border++;
 
-                                // check the next column exist, and check right
+                                /* Check if the down side grid is exist or not. */
                                 if ( j < gridColSize-1 && grid[i][j+1] == 1 )
                                         border++;
                         }
                 }
         }
+
         return (perimeter * 4) - (border * 2);
+}
+
+void print_matrix(int ** matrix, int row, int col)
+{
+	int i, j;
+
+	for ( i = 0; i < row; i++){
+
+		for ( j = 0; j < col; j++ ){
+			printf("%d ", matrix[i][j] );
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+/* A safe free function.  */
+void _safe_free(void **pp)
+{
+	if (pp != NULL && *pp != NULL){
+		free(*pp);
+		*pp = NULL;
+	}
+}
+#define safe_free(p) _safe_free( (void**) &p )
+
+
+void free_matrix( int ** matrix, int row)
+{
+	int i;
+	for (i = 0; i < row; i++)
+		safe_free(matrix[i]);
+
+	safe_free(matrix);
+}
+
+int main()
+{
+        int i;
+        int row = 4;
+        int col = 4;
+
+        int ** grid = (int **) malloc( sizeof(int *) * row );
+        for (i = 0; i < row; i++)
+                grid[i] = (int *) malloc( sizeof(int) * col );
+
+        grid[0][0] = 0;
+        grid[0][1] = 1;
+        grid[0][2] = 0;
+        grid[0][3] = 0;
+        grid[1][0] = 1;
+        grid[1][1] = 1;
+        grid[1][2] = 1;
+        grid[1][3] = 0;
+        grid[2][0] = 0;
+        grid[2][1] = 1;
+        grid[2][2] = 0;
+        grid[2][3] = 0;
+        grid[3][0] = 1;
+        grid[3][1] = 1;
+        grid[3][2] = 0;
+        grid[3][3] = 0;
+
+        print_matrix(grid, row, col);
+
+        printf("perimeter: %d\n", islandPerimeter(grid, row, col));
+
+        free_matrix(grid, row);
+
+        return 0;
 }
