b4ca77a0fce248b8cea21d9c0b885afd4a8ca316
Lin Emsber <linemsber@gmail.com>
2017-07-07 00:00:02 +0800

    Review and add a clear explanation.

--- next_greater_element_I/next_greater_element_I_01.c
+++ next_greater_element_I/next_greater_element_I_01.c
@@ -15,43 +15,52 @@ int * nextGreaterElement(int * findNums, int findNumsSize, int * nums, int numsS
 
 	*returnSize = findNumsSize;
 
-	if(findNums != NULL && findNumsSize != 0 && nums != NULL && numsSize != 0) {
+	if(findNums == NULL || findNumsSize == 0 || nums == NULL || numsSize == 0) {
+		return NULL;
+	}
 
-		/* initalize the return_array */
-		return_array = malloc((*returnSize) * sizeof(int));
-		if(return_array == NULL) {
-                        return NULL;
+	/* 1. Create a array to store the result, and find the max element.
+	 * 2. Create a hash table to save the corresponding result number of each element of the array, findNumsSize.
+	 * 3. Start to compare each the numbers of two arrays from the first element. If find the greater number,
+	 * then break from the inner loop. And record the result into the hash table.
+	 * 4. Convert the hash table to the result of array.
+	 */
+
+	return_array = (int *) malloc( findNumsSize * sizeof(int) );
+	if(return_array == NULL) {
+                return NULL;
+	}
+
+	/* find the max element in the nums */
+	max = 0;
+	for(i = 0; i < numsSize; i++) {
+		if(max < nums[i]) {
+			max = nums[i];
 		}
+	}
 
-		max = 0;
-		for(i = 0; i < numsSize; i++) {
-			if(max < nums[i]) {
-				max = nums[i];
+	nums_hash = (int *) malloc( (max + 1) * sizeof(int) );
+	if(nums_hash == NULL) {
+                return NULL;
+	}
+
+	/* Compare all elements of the array, findNumsSize. */
+	for(i = 0; i < numsSize - 1; i++) {
+
+		nums_hash[nums[i]] = -1;
+		for(j = i + 1; j < numsSize; j++) {
+
+			if(nums[j] > nums[i]) {
+				nums_hash[nums[i]] = nums[j];
+				break;
 			}
 		}
+	}
+	/* The last element. */
+	nums_hash[nums[numsSize-1]] = -1;
 
-		nums_hash = (int *) malloc( (max + 1) * sizeof(int) );
-		if(nums_hash == NULL) {
-                        return NULL;
-		}
-
-		for(i = 0; i < numsSize-1; i++) {
-
-			nums_hash[nums[i]] = -1;
-			for(j = i + 1; j < numsSize; j++) {
-
-				if(nums[j] > nums[i]) {
-					nums_hash[nums[i]] = nums[j];
-					break;
-				}
-			}
-		}
-		nums_hash[nums[numsSize-1]] = -1;
-
-		/* find the next greater element */
-		for(i = 0; i < findNumsSize; i++) {
-			return_array[i] = nums_hash[findNums[i]];
-		}
+	for(i = 0; i < findNumsSize; i++) {
+		return_array[i] = nums_hash[findNums[i]];
 	}
 
 	return return_array;
@@ -60,13 +69,13 @@ int * nextGreaterElement(int * findNums, int findNumsSize, int * nums, int numsS
 int main()
 {
         int i;
-        int num_1[3] = {4, 1, 2};
-        int num_2[4] = {1, 3, 4, 2};
+        int num_1[8] = {4, 15, 2, 3, 9, 5, 11, 10};
+        int num_2[11] = {1, 3, 4, 2, 7, 13, 9, 16, 8, 12, 8};
 
         int * return_num;
         int return_size;
 
-        return_num = nextGreaterElement(num_1, 3, num_2, 4, &return_size);
+        return_num = nextGreaterElement(num_1, 8, num_2, 11, &return_size);
 
         for (i = 0; i < return_size; i++)
                 printf("%d ", return_num[i]);
