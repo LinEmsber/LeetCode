326dbeb3b1692281858130671504edb57b38f5f8
Lin Emsber <linemsber@gmail.com>
2017-08-02 23:24:34 +0800

    Review and improve the code

--- reverse_string/reverse_string_01.c
+++ reverse_string/reverse_string_01.c
@@ -1,26 +1,46 @@
-/* Reverse String */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int get_length(char * s)
+{
+	int length = 0;
+	char * c = s;
+	while(*c != '\0')
+	{
+		c = c + 1;
+		length = length + 1;
+	}
+	return length;
+}
+
+/* Reverse String */
 char* reverseString(char* s)
 {
+	/* The starting position. */
+	int start = 0;
+	/* Find the last char position. */
+	int end =  get_length(s)-1;
 
-        char *start, *end;
-        char tmp;
+	while( start < end ){
+		*(s+start) = *(s+start) ^ *(s+end);
+		*(s+end) = *(s+start) ^ *(s+end);
+		*(s+start) = *(s+start) ^ *(s+end);
 
-        // the starting position.
-        start = s;
-
-    	// find the last char position.
-    	end = s + strlen(s) -1;
-
-    	// reverse
-    	while( start < end ){
-        	tmp = *start;
-        	*start = *end;
-        	*end = tmp;
-	        start++;
-    		end--;
-    	}
+		start++;
+		end--;
+	}
 
 	return s;
+}
 
+int main()
+{
+	char * original_string = "Hello World";
+	char * reverse_string = strdup(original_string);
+
+	printf("Original string: %s\n", original_string);
+	printf("Reverse string: %s\n", reverseString(reverse_string));
+
+	return 0;
 }
