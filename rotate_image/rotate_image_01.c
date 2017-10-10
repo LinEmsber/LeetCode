36b4d330f36420319d32899b4f17558c01be4446
Lin Emsber <linemsber@gmail.com>
2017-10-10 23:58:43 +0800

    Try to finish this

--- rotate_image/rotate_image_01.c
+++ rotate_image/rotate_image_01.c
@@ -11,65 +11,58 @@ void swap(int * a, int * b)
 }
 
 
-// void rotate(int ** matrix, int row, int col)
-// {
-// 	int i, j;
-// 	for(i = 0; i < row; i++){
-// 		// for(j = 0; j < col; j++){
-// 			// swap( &matrix[j][col - i], &matrix[i][j]);
-// 		for(j = i + 1; j < col; j++){
-// 			swap( &matrix[j][i], &matrix[i][j]);
-// 		}
-// 	}
-// }
-
-void rotate(int ** matrix, int row, int col)
+void transpose(int ** matrix, int row, int col)
 {
-	// int i, j;
-	// int n = row << 1;
-	// printf("h2");
-	// for (i = 0; i < n / 2; ++i)
-	// {
-	// 	for (j = i; j < n - 1 - i; ++j)
-	// 	{
-	// 		int tmp = matrix[i][j];
-	// 		matrix[i][j] = matrix[n-j-1][i];
-	// 		matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-	// 		matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-	// 		matrix[j][n-i-1] = tmp;
-	// 	}
-	// }
-
-	unsigned int r = row, c = col;
-
-	for (r = 0; r < row; r++) {
-		for (c = 0; c < col; c++) {
-
-			// printf("c * row + (row - r - 1): %d\n", c * row + (row - r - 1));
-			// *(matrix + c * row + (row - r - 1)) = *(matrix + r * col + c);
-			swap((matrix + c * row + (row - r - 1)), (matrix + r * col + c));
+	unsigned int i, j;
+	int ** pm = matrix;
+	for (i = 0; i < row / 2 ; i++) {
+		for (j = 0; j < col / 2 ; j++) {
+			// swap( (matrix + c * row + col), (matrix + c * col + row) );
+			swap( (int *)&matrix[i][j], (int *)&matrix[j][i]);
+			print_matrix(pm, row, col);
 
 		}
 	}
 }
 
+void reverse(int ** matrix, int row, int col)
+{
+	unsigned int i, j;
+	for (i = 0; i < row; i++) {
+		for (j = 0; j < col; j++) {
+			// swap( (matrix + c * row + col), (matrix + c * col + row) );
+			swap( (int *)&matrix[i][j], (int *)&matrix[j][i]);
+		}
+	}
+}
+
+
+void rotate(int ** matrix, int row, int col)
+{
+	transpose(matrix, row, col);
+	// reverse(matrix, row, col);
+}
+
+
 int ** rand_matrix_generator_int( int row, int col, int max, int min)
 {
-        int i, j;
-        int range = max - min;
-        int ** matrix = matrix = (int **) malloc( row * sizeof(int*) );
+	int i, j;
+	int range = max - min;
+	int ** matrix = matrix = (int **) malloc( row * sizeof(int*) );
 
 	srand( (unsigned) time(NULL) );
 
-        for ( i = 0; i < row; i++ ){
-                matrix[i] = (int *) malloc( col * sizeof(int) );
-                for ( j = 0; j < col; j++ ){
-                        matrix[i][j] = (rand() % range) + min;
-                }
-        }
+	for ( i = 0; i < row; i++ ){
+		matrix[i] = (int *) malloc( col * sizeof(int) );
+		for ( j = 0; j < col; j++ ){
+			matrix[i][j] = (rand() % range) + min;
+		}
+	}
 
-        return matrix;
+	return matrix;
 }
+
+
 void print_matrix(int ** matrix, int row, int col)
 {
         int i, j;
@@ -93,11 +86,11 @@ void free_matrix( int ** matrix, int row)
 
 int main()
 {
-	int ** image = rand_matrix_generator_int(3, 3, 50, 0);
-	print_matrix(image, 3, 3);
+	int ** image = rand_matrix_generator_int(4, 4, 50, 0);
+	print_matrix(image, 4, 4);
 
-	rotate(image, 3, 3);
-	print_matrix(image, 3, 3);
+	rotate(image, 4, 4);
+	print_matrix(image, 4, 4);
 
 	return 0;
 }
