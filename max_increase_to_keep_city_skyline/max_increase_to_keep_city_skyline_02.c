ec85331a8489a81bea3e879f7a802d35c471f578
Lin Emsber <linemsber@gmail.com>
2018-06-14 23:52:20 +0800

    Add the optimized method
    1. Add the optimized method.
    2. Use a test case to execute the algorithm.

--- max_increase_to_keep_city_skyline/max_increase_to_keep_city_skyline_02.c
+++ max_increase_to_keep_city_skyline/max_increase_to_keep_city_skyline_02.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* This is the most intuitive method to obtain the maximum total sum that the height of
- * the buildings can be increased.
- */
 int maxIncreaseKeepingSkyline(int** grid, int gridRowSize, int *gridColSizes)
 {
 	int i, j, ans = 0;
 	int * array_row = (int *) calloc( gridRowSize, sizeof(int) );
 	int * array_col = (int *) calloc( *gridColSizes, sizeof(int) );
-
-	int ** new_grid = (int **) calloc( gridRowSize, sizeof(int*) );
-	for ( i = 0; i < gridRowSize; i++ )
-		new_grid[i] = (int *) calloc ( *gridColSizes, sizeof(int) );
 
 	/* Find the highest buildings in the line of each rows. */
 	for( i = 0; i < gridRowSize; i++ )
@@ -26,15 +19,12 @@ int maxIncreaseKeepingSkyline(int** grid, int gridRowSize, int *gridColSizes)
 			if( array_col[j] < grid[i][j] )
 				array_col[j] = grid[i][j];
 
-	/* Fill each new grid with the allowed highest floors */
+	/* 1. The allowed increased floors is the less value between its highest value of row and column.
+	 * 2. Sum of each differece value between the original grid and the new grid.
+	 */
 	for(i = 0; i< gridRowSize; i++ )
 		for( j = 0; j < *gridColSizes; j++ )
-			new_grid[i][j] = array_row[i] < array_col[j] ? array_row[i] : array_col[j];
-
-	/* Use the new grid to substract the original grid to obtain the total sum. */
-	for(i = 0; i< gridRowSize; i++ )
-		for( j = 0; j < *gridColSizes; j++ )
-			ans += new_grid[i][j] - grid[i][j];
+			ans += (array_row[i] < array_col[j] ? array_row[i] : array_col[j]) - grid[i][j];
 
 	return ans;
 }
