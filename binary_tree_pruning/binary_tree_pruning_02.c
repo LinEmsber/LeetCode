4dc5a104303a19f1168b5562944ca4217d1aceae
Lin Emsber <linemsber@gmail.com>
2018-06-20 23:35:27 +0800

    Another approach

---
+++ binary_tree_pruning/binary_tree_pruning_02.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode * pruneTree(struct TreeNode * root)
+{
+	if (!root)
+		return root;
+
+	root->left = pruneTree(root->left);
+	root->right = pruneTree(root->right);
+
+	return (!root->val && !root->left && !root->right) ? NULL : root;
+}
