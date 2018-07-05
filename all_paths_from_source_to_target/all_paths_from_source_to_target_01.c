ed1fc5184bc43548385df7e4cf6e7d3223e80713
Lin Emsber <linemsber@gmail.com>
2018-07-05 23:30:50 +0800

    Review and add comment

--- all_paths_from_source_to_target/all_paths_from_source_to_target_01.c
+++ all_paths_from_source_to_target/all_paths_from_source_to_target_01.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *columnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
 
 /* The depth-first search. */
 void dfs(int ** graph, int row, int * col, int * count, int ** columnSizes, int * path, int ** answer, int pos)
@@ -25,34 +20,53 @@ void dfs(int ** graph, int row, int * col, int * count, int ** columnSizes, int 
 	}
 }
 
+/*
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
 int ** allPathsSourceTarget(int ** graph, int graphRowSize, int * graphColSizes, int ** columnSizes, int * returnSize)
 {
-	int i, count = 0;
-	int ** answer = (int**) malloc( sizeof(int*) * 7000 );
-	int * path = (int*) malloc( sizeof(int) * graphRowSize );
+	/* Create the array of arrays and count */
+	int count = 0;
+	int ** array_of_arrays = (int**) malloc( sizeof(int*) * 10000 );
+	int * each_array = (int*) malloc( sizeof(int) * graphRowSize );
 
-	*columnSizes = (int*) malloc( sizeof(int) * 7000 );
+	/* Initialize the array of arrays. */
+	for( int i = 0; i < 10000; i++ )
+		array_of_arrays[i] = (int*) malloc( sizeof(int) * graphRowSize );
+	for( int i = 0; i < graphRowSize; i++ )
+		each_array[i] = NULL;
 
-	for( i = 0; i < 7000; ++i )
-		answer[i] = (int*) malloc( sizeof(int) * graphRowSize );
-	path[0] = 0;
+	/* The sizes of each_array are returnd as *columnSizes array. */
+	*columnSizes = (int*) malloc( sizeof(int) * 10000 );
 
-	dfs(graph, graphRowSize, graphColSizes, &count, columnSizes, path, answer, 0);
+	/* Conduct DFS. */
+	dfs(graph, graphRowSize, graphColSizes, &count, columnSizes, each_array, array_of_arrays, 0);
+
 	*returnSize = count;
 
-	return answer;
+	return array_of_arrays;
 }
 
 int main()
 {
+	int i, j;
 	int * graph_object_ptr = NULL;
-	int graph_object[4][3] = { {1, 2, 0}, {3, 0, 0}, {3, 0, 0}, {0, 0, 0} };
+	int graph_object[4][2] = { {1, 2}, {3, 0}, {3, 0}, {0, 0} };
+
+	for ( i = 0; i < 4; i++ ) {
+		for ( j = 0; j < 2; j++ ) {
+			printf("graph_object[%d][%d] = %d\n", i,j, graph_object[i][j] );
+		}
+	}
+
 	graph_object_ptr = &graph_object[0][0];
 
-	int graph_row_size = 3;
+	int graph_row_size = 4;
 
 	int * graph_col_size_ptr = NULL;
-	int graph_col_size = 3;
+	int graph_col_size = 2;
 	graph_col_size_ptr = &graph_col_size;
 
 	int ** col_size_ptr = NULL;
